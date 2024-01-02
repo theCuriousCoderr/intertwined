7597ec133d115b015dce92cd9b311cf586614631
Olalekan Oladimeji <elijahdimeji549@gmail.com>
2024-01-03 00:21:11 +0100

    Reverting changes

--- sync-hub-be/index.js
+++ sync-hub-be/index.js
@@ -12,7 +12,7 @@ import { Requests } from "./models/requestsModel.js";
 import { ChatHistory } from "./models/chatHistoryModel.js";
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000 ;
 
 const app = express();
 app.use(
